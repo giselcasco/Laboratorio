9c557878987f43ece0f62a155f6ea59659dd9e09
VanyAraujo <araujo.g.vanina@gmail.com>
2018-09-25 19:21:25 -0300

    CHECK punto4

--- app/src/main/java/ar/edu/utn/frsf/dam/isi/laboratorio02/ListaProductosActivity.java
+++ app/src/main/java/ar/edu/utn/frsf/dam/isi/laboratorio02/ListaProductosActivity.java
@@ -1,6 +1,6 @@
 package ar.edu.utn.frsf.dam.isi.laboratorio02;
-import android.os.Bundle;
 import android.content.Intent;
+import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.AdapterView;
@@ -9,8 +9,6 @@ import android.widget.Button;
 import android.widget.ListView;
 import android.widget.Spinner;
 import android.widget.TextView;
-
-import java.util.List;
 
 import ar.edu.utn.frsf.dam.isi.laboratorio02.dao.ProductoRepository;
 import ar.edu.utn.frsf.dam.isi.laboratorio02.modelo.Categoria;
@@ -22,38 +20,37 @@ public class ListaProductosActivity extends AppCompatActivity {
     // para que se visualicen las categorias de los productos
     private Spinner spinner;
     private ArrayAdapter<Categoria> adapterCategoria;
-    private TextView tvProducto;
-    private ListView listaProductos;
+    private TextView tvcategoria;
+    private ListView listCategoria;
     private ProductoRepository listaCategoria;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        setContentView(R.layout.activity_activity);
-        spinner = (Spinner) findViewById(R.id.spinnerProductosCategoria);
-        adapterCategoria = new ArrayAdapter<Categoria>(this,
-                android.R.layout.simple_spinner_item,listaCategoria.getCategorias());
+        setContentView(R.layout.activity_spinner);
+        spinner = (Spinner)findViewById(R.id.spinnerProductosCategoria);
+        adapterCategoria = new ArrayAdapter<Categoria>(this, android.R.layout.simple_spinner_item,listaCategoria.getCategorias());
         adapterCategoria.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter(adapterCategoria);
 
-       /* Button btn2 = (Button) findViewById(R.id.btnProdAddPedido);
+       Button btn2 = (Button) findViewById(R.id.btnProdAddPedido);
         btn2.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Intent intent2 = new Intent ();
                 startActivity(intent2);
             }
-        });*/
+        });
 
         //e)
-      /*  listaProductos.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+      listCategoria.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
             public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Producto producto = (Producto) adapterProducto.getItem(position);
-                tvProducto.setText(producto.getNombre());
+                Categoria categoria = (Categoria)adapterCategoria.getItem(position);
+                tvcategoria.setText(categoria.getNombre());
             }
-        });*/
+        });
 
     }
 
